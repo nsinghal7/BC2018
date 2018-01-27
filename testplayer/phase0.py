28f772af72b33e82da4a8dd6965ad46c9d2930ca
Christopher Chang <christopherchang9@gmail.com>
2018-01-27 16:07:55 -0500

    hi2

--- testplayer/phase0.py
+++ testplayer/phase0.py
@@ -5,5 +5,5 @@ def earth_karbonite_search(self):
     for row in range(self.start_map.height):
         for col in range(self.start_map.width):
             loc = bc.MapLocation(self.planet, row, col)
-            
+            self.
     return [[self.start_map.initial_karbonite_at(bc.MapLocation(self.planet, row, col)) for col in range(self.start_map.width)] for row in range(self.start_map.height)]

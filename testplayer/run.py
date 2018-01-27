f060d78508777e2ddd04b0b651775157e35d003b
Christopher Chang <christopherchang9@gmail.com>
2018-01-27 16:06:18 -0500

    hi

--- testplayer/run.py
+++ testplayer/run.py
@@ -4,9 +4,12 @@ import sys
 import traceback
 import time
 import math
-from phase0 import earth_karbonite_search
-from phase1 import replicate_workers_phase
+import phase0
+import phase1
 from utilities import Point
+from utilities import KarbCluster
+from utilities import Container
+from utilities import UnitInfo
 
 import os
 
@@ -28,25 +31,36 @@ def main():
     # aside from turns taking slightly different amounts of time due to noise.
     random.seed(6137)
     
-    self.dir_to_dxdy = [Point(-1, 0), Point(-1, 1), Point(0, 1), Point(1, 1), Point(1, 0), Point(1, -1), Point(0, -1), Point(-1, -1), Point(0, 0)]
-    self.dxdy_to_dir = [[bc.Direction.Northwest, bc.Direction.North, bc.Direction.Northeast],
-                        [bc.Direction.West, bc.Direction.Center, bc.Direction.East],
-                        [bc.Direction.Southwest, bc.Direction.South, bc.Direction.Southeast]]
+    # Ex: self.dir_to_dxdy[bc.Direction.Southeast.value] => Point(-1, 1)
+    self.dir_to_dxdy = [Point(1, 0), Point(1, 1), Point(0, 1), Point(-1, 1), Point(-1, 0), Point(-1, -1), Point(0, -1), Point(1, -1), Point(0, 0)]
+    # Ex: self.dxdy_to_dir[-1][1] => bc.Direction.Southeast
+    self.dxdy_to_dir = [[bc.Direction.Center, bc.Direction.East, bc.Direction.West],
+                        [bc.Direction.North, bc.Direction.Northeast, bc.Direction.Northwest],
+                        [bc.Direction.South, bc.Direction.Southeast, bc.Direction.Southwest]]
     self.directions = self.dir_to_dxdy[:-1]
     
     self.planet = self.gc.planet()
     self.start_map = self.gc.starting_map(planet)
     self.team = self.gc.team()
     self.asteroids = self.gc.asteroid_pattern()
-    self.orbit = 
+    self.orbit = self.gc.orbit_pattern()
     
+    if self.planet == bc.Planet.Earth:
+        main_earth(self)
+    else:
+        main_mars(self)
 
+def main_earth(self):
     # let's start off with some research!
     # we can queue as much as we want.
     self.gc.queue_research(bc.UnitType.Worker) #more harvest 25
     self.gc.queue_research(bc.UnitType.Worker) #more build 100
     self.gc.queue_research(bc.UnitType.Mage) #more damage 125
 
-    self.karb_clusters, self.neighbors = earth_karbonite_search(self)
-    replicate_workers_phase(self)
+    self.karb_clusters, self.neighbors = phase0.earth_karbonite_search(self)
+    phase1.replicate_workers_phase(self)
     #TODO
+    
+
+if __name__ == '__main__':
+    main()

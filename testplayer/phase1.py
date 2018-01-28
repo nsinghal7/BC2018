8a0f20bb8a1e29b4f898c4b76d51df6315844309
nsinghal7 <nmsinghal7@gmail.com>
2018-01-28 00:50:03 -0500

    Add controls for B group in phase 1

--- testplayer/phase1.py
+++ testplayer/phase1.py
@@ -1,15 +1,22 @@
-import utilities
+from utilities import try_nearby_directions
+from utilities import Path
+from utilities import Point
 import battlecode as bc
+import random
 
 PHASE1_WORKERS_WANTED = 10
 ABILITY_COOLDOWN_LIMIT = 10
 FACTORY_BUILD_COST = 100
+KARBONITE_FOR_REPLICATE = 30
 
 def replicate_workers_phase(state):
     units = state.gc.units()
-    factory_loc = None #TODO
     replicate_id = units[0].id
     cluster_index = 0
+    factory_path = None
+    fp_start = None
+    factory_loc = None
+    stuck = False
     score = -1
     for unit in units:
         for index in range(len(state.karb_clusters)):
@@ -18,72 +25,127 @@ def replicate_workers_phase(state):
                 replicate_id, cluster_index, score = unit.id, index, ns
     for unit in units:
         unit.info().is_B_group = (unit.id == replicate_id)
+        unit.info().prevdir = bc.Direction.North
 
-    begin_building = False
-    blueprint_laid = False
-
-    while True:
+    while len(units) < PHASE1_WORKERS_WANTED:
         index = 0
         while index < len(units):
+            unit = units[index]
+            ml = unit.location.map_location()
             if unit.info().is_B_group:
-                ml = unit.location.map_location()
-                goal = state.karb_clusters[cluster_index][ml.y][ml.x]
-                skip_exact = goal[1] == 1
-                # move and determine direction to replicate
-                if goal[0] == bc.Direction.Center: #shouldn't happen
-                    for direction in try_nearby_directions(bc.Direction.North):
-                        if gc.can_move(unit.id, direction):
-                            gc.move_robot(unit.id, direction)
-                            ml = ml.add(direction)
-                            goal = direction.opposite(), 1
-                            skip_exact = True
-                            break
-                    if goal == bc.Direction.Center:
-                        goal = bc.Direction.North, 0
-                        skip_exact = False
-                else:
-                    for direction in try_nearby_directions(goal[0], skip_exact):
-                        if gc.can_move(unit.id, direction):
-                            gc.move_robot(unit.id, direction)
-                            ml = ml.add(direction)
-                            skip_exact = goal[1] <= 2
-                            if goal[1] == 1:
-                                #need new direction
-                                goal = state.karb_clusters[cluster_index][ml.y][ml.x]
-                            break
-
-                # replicate if can
-                if unit.ability_cooldown() < ABILITY_COOLDOWN_LIMIT and len(units) < PHASE1_WORKERS_WANTED:
-                    for direction in try_nearby_directions(goal[0], skip_exact):
-                        if gc.can_replicate(unit.id, direction):
-                            gc.replicate(unit.id, direction)
-                            units.append(gc.sense_unit_at_location(ml.add(direction)))
-                            break
-                begin_building = len(units) >= PHASE1_WORKERS_WANTED
-
-
-                bml = ml.add(goal[0])
-                if blueprint_laid and goal[1] == 1:
-                    bp = gc.sense_unit_at_location(bml)
-                    if gc.can_build(unit.id, bp.id):
-                        gc.build(unit.id, bp.id)
+                path = unit.info().cluster_path
+                goal = state.karb_clusters[cluster_index][ml.y][ml.x][0]
+                if path is None and (goal.x != 0 or goal.y != 0):
+                    # not there yet
+                    if state.gc.is_move_ready(unit.id):
+                        for direction in try_nearby_directions(goal.to_Direction()):
+                            if state.gc.can_move(unit.id, direction):
+                                state.gc.move_robot(unit.id, direction)
+                                unit.info().prevdir = direction
+                                break
+                    try_harvest(unit, (-goal).to_Direction())
+                elif path is None:
+                    # is there, but hasn't chosen path yet
+                    neighbors = state.neighbors[ml.y][ml.x]
+                    if factory_loc is None:
+                        # is the first one to reach the cluster, should set factory location
+                        q = [Path(Point(ml.y, ml.x))]
+                        index = 0
+                        while index < len(q):
+                            current = q[index]
+                            index += 1
+                            if factory_loc_check_update(state, bc.MapLocation(current.dest.x, current.dest.y)):
+                                # factory should be here, maps already updated
+                                factory_loc = current.dest.y, current.dest.x
+                                unit.info().cluster_path = current.steps.reverse()
+                                factory_path = current.steps.reverse()
+                                fp_start = ml
+                                unit.info().cluster_dest_factory = True
+                                break
+                            for dy in range(-1, 2):
+                                y = current.dest.y + dy
+                                if not (0 <= y < len(state.kmap)):
+                                    continue
+                                for dx in range(-1, 2):
+                                    x = current.dest.x + dx
+                                    if 0 <= x < len(state.kmap[0]):
+                                        q.append(current + Point(dy, dx))
+                        if factory_loc is None:
+                            raise Exception("There is absolutely nowhere to put a factory that doesn't ruin things")
+                    elif len(neighbors) == 0:
+                        unit.info().cluster_path = factory_path #the only place you could be is the only square in the cluster
+                        unit.info().cluster_dest_factory = True
                     else:
-                        raise Exception("Can't build even though I think I should")
-                elif begin_building and gc.karbonite() > FACTORY_BUILD_COST and gc.karbonite_at(bml) < 3 and goal[1] == 1:
-                    if gc.can_blueprint(unit.id, bc.UnitType.Factory, goal[0]):
-                        gc.blueprint(unit.id, bc.UnitType.Factory, goal[0])
+                        unit.info().cluster_path = random.choice(neighbors).steps
+                        unit.info().cluster_dest_factory = False
+
+                path = unit.info().cluster_path
+                if path is not None and state.gc.is_move_ready(unit.id) and unit.info().prevdir is not None:
+                    if len(path) == 0:
+                        # move out of the way! go past the square in same direction as before hopefully
+                        o = bc.Direction.North
+                        moved = False
+                        for direction in try_nearby_directions(unit.info().prevdir):
+                            if state.gc.can_move(unit.id, direction):
+                                state.gc.move_robot(unit.id, direction)
+                                unit.info().prevdir = None
+                                o = direction.opposite()
+                                unit.info().cluster_path = [(o.dy(), o.dx())]
+                                moved = True
+                                break
+                        if not moved and unit.info().cluster_dest_factory:
+                            stuck = True
+                        #harvest goal location or nearby
+                        try_harvest(unit, o)
+                    elif stuck:
+                        unit.info().is_B_group = False
                     else:
-                        raise Exception("Can't blueprint even though I think I should")
-                else:
-                    for direction in try_nearby_directions(goal[0]):
-                        if gc.can_harvest(unit.id, direction):
-                            gc.harvest(unit.id, direction)
+                        goal = path.pop()
+                        for direction in try_nearby_directions(goal.to_Direction()):
+                            if state.gc.can_move(unit.id, direction):
+                                state.gc.move_robot(unit.id, direction)
+                                unit.info().prevdir = direction
+                                if direction != goal:
+                                    path.append(-goal)
+                                break
+                        try_harvest(unit, direction.opposite())
+                elif path is not None:
+                    # harvest in factory after passing it
+                    try_harvest(unit, path[-1].to_Direction())
+
+                if unit.ability_heat() < ABILITY_COOLDOWN_LIMIT and len(units) < PHASE1_WORKERS_WANTED and state.gc.karbonite() > KARBONITE_FOR_REPLICATE:
+                    if len(path) == 0:
+                        goal = unit.info().prevdir
+                    else:
+                        goal = path[-1]
+                    for direction in try_nearby_directions(goal):
+                        if state.gc.can_replicate(unit.id, direction):
+                            state.gc.replicate(unit.id, direction)
+                            new = state.gc.sense_unit_at_location(ml.add(direction))
+                            ui = new.info()
+                            ui.is_B_group = True
+                            ui.prevdir = direction
+                            ui.cluster_path = path[:-1]
+                            ui.cluster_dest_factory = unit.info().cluster_dest_factory
+                            units.append(new)
                             break
             else:
-                # control non-group-b units
-
+                # not B group
+                pass
+            index += 1
+        state.gc.next_turn()
+        units = state.gc.units()
+                
+def try_harvest(unit, goal):
+    for direction in try_nearby_directions(bc.Direction.North):
+        if state.gc.can_harvest(unit.id, direction):
+            state.gc.harvest(unit.id, direction)
+            return True
+    return False
 
 def cluster_worker_score(ml, cluster):
     dist = cluster[ml.y][ml.x][1]
     val = cluster.karb
+    if dist == 0:
+        return 2 * val
     return val / dist ** 2
